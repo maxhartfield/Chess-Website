9f3617240af598a70cafb10b3ce79f628e68fd90
maxhartfield <maxhartfield@gmail.com>
2023-10-24 15:36:00 -0500

    board editor

--- chess.js
+++ chess.js
@@ -141,7 +141,7 @@ function boardEditor(event) {
     } else {
         for(var i = 0; i < 8; i++) {
             for(var j = 0; j < 8; j++) {
-                chessGrid[i][j] = charMap.get(document.getElementById(getCoordinates(i, j)).textContent)
+                chessGrid[i][j] = charMap.get(document.getElementById(getCoordinates(i, j)).innerHTML)
             }
         }
         var king = findKings()
